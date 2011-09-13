ce4934512bb341328858fd800880d2aa79c7507d
Song Wei <1983.song.wei@gmail.com>
2011-09-12 23:29:25 -0500

    support mutiple connected devices in run.sh

--- zygote/run.sh
+++ zygote/run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+device=HT9A4LV01921
+set -x
 /Users/song/adk/android-ndk-r6b/ndk-build
-adb shell mkdir -p /tmp/
-adb push ../dalvik/foo.jar /tmp/
-adb push libs/armeabi/dvz-jni /tmp/
-adb shell chmod a+x /tmp/dvz-jni
-#adb shell dalvikvm -cp /tmp/foo.jar Foo #this one won't work if you need android framework libraries. you need to ask zygote to initialze the env for you. one way is to use dvz utility.
-#adb shell /tmp/dvz-jni --help
-adb shell /tmp/dvz-jni -classpath /tmp/foo.jar Foo
+adb -s $device shell mkdir -p /data/local/tmp/
+adb -s $device push ../dalvik/foo.jar /data/local/tmp/
+adb -s $device push libs/armeabi/dvz-jni /data/local/tmp/
+adb -s $device shell chmod a+x /data/local/tmp/dvz-jni
+#adb -s $device shell dalvikvm -cp /data/local/tmp/foo.jar Foo #this one won't work if you need android framework libraries. you need to ask zygote to initialze the env for you. one way is to use dvz utility.
+#adb -s $device shell /data/local/tmp/dvz-jni --help
+adb -s $device shell /data/local/tmp/dvz-jni -classpath /data/local/tmp/foo.jar Foo
