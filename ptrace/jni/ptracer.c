aaf2d8c4d1745c18261b0b61ad29b36ad4336906
Song Wei <1983.song.wei@gmail.com>
2011-09-16 22:32:06 -0500

    single tracer

--- ptrace/jni/ptracer.c
+++ ptrace/jni/ptracer.c
@@ -4,22 +4,92 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <linux/user.h>
-int main()
-{   pid_t child;
-  long orig_eax;
-  child = fork();
-  if(child == 0) {
-    ptrace(PTRACE_TRACEME, 0, NULL, NULL);
-    execl("/bin/ls", "ls", NULL);
+#include <signal.h>
+
+int main(int argc, char ** argv[])
+{  
+  struct pt_regs u;
+  pid_t child;
+  pid_t wait_child;
+  int status;
+  int i;
+  int steps;
+  long w;
+  siginfo_t sig;
+  void * addr;
+  if(argc != 2){
+    printf("ptracer pid\n");
+    return 0;
   }
-  else {
-    wait(NULL);
-    orig_eax = ptrace(PTRACE_PEEKUSER,
-		      child, 4 * 0,
-		      NULL);
-    printf("The child made a "
-	   "system call %ld\n", orig_eax);
-    ptrace(PTRACE_CONT, child, NULL, NULL);
+  child = atoi(argv[1]);
+  printf("tracing %d\n", child);
+  steps=0;
+  w = ptrace(PTRACE_ATTACH, child, NULL, NULL);
+  printf("ptrace PTRACE_ATTACH returns:%d\n", w);
+  if(w < 0){
+    return 0;
   }
+  while(1) {
+    steps++;
+    //printf("waiting for child %d\n", child);
+    wait_child = wait( &status );
+    if(wait_child != child){
+      printf("child:%d, wait_child:%d, they don't match\n", child, wait_child);
+      return 0;
+    }
+    if(WIFEXITED( status )){
+      printf("child:%d exited with code %d\n", child, WEXITSTATUS(status));
+      break;
+    }
+    /* if (WIFSTOPPED( status )){ */
+    /*   printf( "Child has stopped due to signal %d\n", */
+    /* 	      WSTOPSIG( status ) ); */
+    /* } */
+    /* if(WIFSIGNALED(status)){ */
+    /*   printf( "Child %ld received signal %d\n", */
+    /* 	      (long)child, */
+    /* 	      WTERMSIG(status) ); */
+    /* } */
+    /* addr = (void*)32; */
+    /* w = ptrace(PTRACE_PEEKTEXT, */
+    /* 	       child, addr, */
+    /* 	       NULL); */
+    /* printf("content at address 0%x:%x\n", addr, w); */
+    /* w = ptrace(PTRACE_GETSIGINFO, */
+    /* 	       child, NULL, */
+    /* 	       &sig); */
+    /* printf("ptrace PTRACE_GETSIGINFO returns:%d\n", w); */
+    /* printf("si_signo %d, si_code %d\n", sig.si_signo, sig.si_code); */
+    memset(&u, 0, sizeof(u));
+    w = ptrace(PTRACE_GETREGS,
+	       child, NULL,
+	       &u);
+    if(w){
+      printf("ptrace PTRACE_GETREGS returns:%d\n", w);
+      goto detach;
+    }
+    /* for(i=0; i<18; i++){       */
+    /*   printf("%08x ", u.uregs[i]); */
+    /* } */
+    printf("pc:0x%x\n", u.ARM_pc);
+    if(steps<0){
+      w = ptrace(PTRACE_SYSCALL,
+      		 child, NULL,
+      		 NULL);
+      printf("ptrace PTRACE_SINGLESTEP returns:%d\n", w);
+      /* w = ptrace(PTRACE_CONT, */
+      /* 		 child, NULL, */
+      /* 		 NULL); */
+      /* if(w){ */
+      /* 	printf("ptrace PTRACE_CONT returns:%d\n", w); */
+      /* 	goto detach; */
+      /* } */
+    }else{
+      break;
+    }
+  }
+ detach:
+  w = ptrace(PTRACE_DETACH, child, NULL, NULL);
+  printf("ptrace PTRACE_DETACH returns:%d\n", w);
   return 0;
 }
