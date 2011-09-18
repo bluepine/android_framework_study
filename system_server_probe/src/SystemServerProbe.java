0cb194db081e38eca444f0ff0a593805a8cce0c2
Song Wei <1983.song.wei@gmail.com>
2011-09-18 18:42:53 -0500

    run dalvikvm before dvz to extract dex files to cache

--- system_server_probe/src/SystemServerProbe.java
+++ system_server_probe/src/SystemServerProbe.java
@@ -1,6 +1,7 @@
 import android.os.Build;
 class SystemServerProbe {
     public static void main(String[] args) {
-	System.out.println("testing android runtime: sdk int:"+Build.VERSION.SDK_INT+"\n"); 
+	//	System.out.println("testing android runtime: sdk int:"+Build.VERSION.SDK_INT+"\n"); 
+	System.out.println("si:"+Build.VERSION.SDK_INT+"\n"); 
     }
 }
