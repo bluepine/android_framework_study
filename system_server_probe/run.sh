51e161fa860ca251304e257829ef72d52b1f0ea2
Song Wei <1983.song.wei@gmail.com>
2011-09-18 18:30:45 -0500

    system server probe init

---
+++ system_server_probe/run.sh
@@ -0,0 +1,9 @@
+echo hi
+device=HT9A4LV01921
+#device=033c20c142017617
+set -x
+adb -s $device shell mkdir -p /mnt/sdcard/tmp
+adb -s $device push build/jar/ssp.jar /mnt/sdcard/tmp
+adb -s $device shell dvz -classpath /mnt/sdcard/tmp/ssp.jar SystemServerProbe
+#adb -s $device shell app_process -classpath /mnt/sdcard/tmp/ssp.jar / SystemServerProbe
+#adb -s $device shell app_process  /mnt/sdcard/tmp/  SystemServerProbe
---
+++ system_server_probe/src/SystemServerProbe.java
@@ -0,0 +1,6 @@
+import android.os.Build;
+class SystemServerProbe {
+    public static void main(String[] args) {
+	System.out.println("testing android runtime: sdk int:"+Build.VERSION.SDK_INT+"\n"); 
+    }
+}
